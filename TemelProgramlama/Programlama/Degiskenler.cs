d5107a7825bb8d9906fcef5249848f009d24487b
İbrahim Serkan Sevtekin <serkansevtekin@gmail.com>
2025-08-26 00:52:10 +0300

    Degiskenler

---
+++ TemelProgramlama/Programlama/Degiskenler.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Programlama.DegiskenlerNameSpace
+{
+    public class Degiskenler
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi = 2;
+            System.Console.WriteLine(sayi);
+            System.Console.WriteLine(sayi + 2);
+            System.Console.WriteLine(sayi * sayi);
+            System.Console.WriteLine(sayi -5);
+            Console.ReadKey();
+        }
+
+        private static void DegiskenBasitOrnek()
+        {
+               //Tip
+            /*   int sayi1 = 0;
+              float pi = 3.14f;
+              char secim = 'e';
+              string isim = "serkan";
+              bool dogruMu = false; */
+
+            //Atama
+            //string isim = "serkan";
+            string isim;
+            isim = "Serkan";
+            Console.WriteLine("Merhaba " + isim + ".");
+            isim = "Mehmet";
+            Console.WriteLine("Merhaba Sayın " + isim +".");
+
+        }
+    }
+}
