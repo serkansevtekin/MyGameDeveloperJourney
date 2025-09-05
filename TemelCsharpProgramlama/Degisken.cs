a88e5babbbed94a0a4a97532ff003277602f71ef
İbrahim Serkan Sevtekin <serkansevtekin@gmail.com>
2025-09-05 23:42:48 +0300

    Temel C# Programlam Kurs Kalsör Ayarı

---
+++ TemelCsharpProgramlama/Degisken.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TemelCSharp
+{
+     class DegiskenlerClass
+    {
+        public static void DegiskenlerMainMethod()
+        {
+            System.Console.WriteLine("Ananan");
+        }
+    }
+}
---
+++ TemelCsharpProgramlama/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemelCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            TemelCSharp.DegiskenlerClass.DegiskenlerMainMethod();
+        }
+    }
+}
