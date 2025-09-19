605b5cfa29244737ca2c1810c8c6c49d7e3108e3
İbrahim Serkan Sevtekin <serkansevtekin@gmail.com>
2025-09-20 00:55:33 +0300

    Orta Seviye Algoritma Eğitimi Bitiş

--- Algoritma/obj/Debug/net9.0/Algoritma.AssemblyInfo.cs
+++ Algoritma/obj/Debug/net9.0/Algoritma.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algoritma")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f7666a0c95462c3623982c6788a8aff6028aaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea188c105a1051d3d0fc9996193554acf3d7645f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algoritma")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algoritma")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
