9acd527b3fe3880424efec2b114b4d2d268322ba
İbrahim Serkan Sevtekin <serkansevtekin@gmail.com>
2025-09-25 23:29:44 +0300

    AddFirst and AddLast

--- Algoritma/IleriSeviye/DiziVeKoleksiyonlar/LinkedLists/SinglyLinkedList.cs
+++ Algoritma/IleriSeviye/DiziVeKoleksiyonlar/LinkedLists/SinglyLinkedList.cs
@@ -7,9 +7,18 @@ namespace Programlama.IleriAlgoritma
         //Tek Yönlü Bağlı Liste
         public static void SinglyLinkedListRunMain()
         {
+            var linkedlist = new SinglyLinkedList<int>();
+            //3 , 2 , 1
+            linkedlist.AddFirst(1);
+            linkedlist.AddFirst(2);
+            linkedlist.AddFirst(3);
+            
+            // 3 , 2 , 1 , 5 , 6
+            linkedlist.AddLast(5);
+            linkedlist.AddLast(6);
 
         }
-     }
+    }
 
 
     #region Node Design (Düğüm Tasarımı)
@@ -21,9 +30,42 @@ namespace Programlama.IleriAlgoritma
         public NodeDesign(T value)
         {
             Value = value;
-       
+
         }
         public override string ToString() => $"{Value}";
+    }
+    #endregion
+
+    #region Tek yönlü bağlı listede AddFirst işlevi | Liste Başına Ekleme
+    internal class SinglyLinkedList<T>
+    {
+        public NodeDesign<T>? Head { get; set; }
+
+        public void AddFirst(T value)
+        {
+
+            var newNode = new NodeDesign<T>(value);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        public void AddLast(T value)
+        {
+            var newNode = new NodeDesign<T>(value);
+            if (Head == null)
+            {
+                Head = newNode;
+                return;
+            }
+            var current = Head;
+            while (current!.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode; //newNode;
+        }
+
+
     }
     #endregion
 
