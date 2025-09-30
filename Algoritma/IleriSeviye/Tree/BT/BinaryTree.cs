a8f8b4134c000eb1949b7ced2d8eb6c2bc7627ac
İbrahim Serkan Sevtekin <serkansevtekin@gmail.com>
2025-09-30 16:14:33 +0300

    Recursive In Order, Pre Order ve Post Order

--- Algoritma/IleriSeviye/Tree/BT/BinaryTree.cs
+++ Algoritma/IleriSeviye/Tree/BT/BinaryTree.cs
@@ -5,7 +5,7 @@ namespace Programlama.IleriAlgoritma.Tree
     //Generic BinaryTree sınıfı, T tipi IComparable olmalı
     public class BinaryTree<T> where T : IComparable
     {
-        #region In Order Dolaşım Özyineleme (recursion)
+        #region InOrder Dolaşım Özyineleme (Recursive)
         //Public metot: In-Order traversal yapar ve sonucu List<NodeTree<T>> olarak döner
         public List<NodeTree<T>> InOrder(NodeTree<T>? root)
         {
@@ -37,9 +37,98 @@ namespace Programlama.IleriAlgoritma.Tree
             }
         }
         #endregion
-   
-        #region  Preorder Dolaşım Özyineleme (recursion)
-            
+
+        #region  PreOrder Dolaşım Özyineleme (Recursive)
+        public List<NodeTree<T>> PreOrder(NodeTree<T>? root)
+        {
+            var list = new List<NodeTree<T>>();
+            PreOrderHelper(root, list);
+            return list;
+        }
+
+        private void PreOrderHelper(NodeTree<T>? root, List<NodeTree<T>> list)
+        {
+            if (root != null)
+            {
+                list.Add(root);
+                PreOrderHelper(root.Left, list);
+                PreOrderHelper(root.Right, list);
+            }
+        }
+
         #endregion
+
+        #region PostOrder Dolaşım Özyineleme (Recursive)
+        public List<NodeTree<T>> PostOrder(NodeTree<T>? root)
+        {
+            var list = new List<NodeTree<T>>();
+            PostOrderHelper(root, list);
+            return list;
+        }
+        private void PostOrderHelper(NodeTree<T>? root, List<NodeTree<T>> list)
+        {
+            if (root != null)
+            {
+                PostOrderHelper(root.Left, list);
+                PostOrderHelper(root.Right, list);
+                list.Add(root);
+            }
+        }
+        #endregion
+
+
+        #region Non Recursive InOrder Traversal
+        public List<NodeTree<T>> InOrderNonRecursiveTreversal(NodeTree<T> root)
+        {
+            // Sonuçları saklayacağımız list
+            var list = new List<NodeTree<T>>();
+
+            // Ziyaret edilecek düğümleri tutmak için stack (LIFO)
+            var s = new Stack<NodeTree<T>>();
+
+            //Başlangıç noktası: kök düğüm
+            NodeTree<T> currentNode = root;
+
+            //Stack boş değilse veye ilerleyecek bir node varsa devam et
+            while (s.Count > 0 || currentNode != null)
+            {
+                //1. Adım: Sol alt ağaç boyunca ilerle, her düğümü stack'e ekle
+                while (currentNode != null)
+                {
+                    s.Push(currentNode);                // mevcut düğümü stack'e ekle
+                    currentNode = currentNode.Left!;    // sol çocuğa git
+                }
+
+                //2. Adım: En soldaki node'a ulaşıldı, şimdi stacten geri çık
+                currentNode = s.Pop();  //Stack'ten bir düğüm çıkar
+                list.Add(currentNode);  // O düğümü listeye ekle (ziyaret et)
+
+                //3. Adım: Sağ alt ağacı gezmek için sağ alt çocuğa git
+                currentNode = currentNode.Right!;
+            }
+            // In-Order sıralanmış listeyi döndür
+            return list;
+        }
+
+        #endregion
+
+        #region Non Recursive PreOrder Traversal
+        public List<NodeTree<T>> PreOrderNoneRecursiveTreversal(NodeTree<T> root)
+        {
+            var list = new List<NodeTree<T>>();
+            var s = new Stack<NodeTree<T>>();
+            
+
+            return list;
+        }
+
+        #endregion
+
+        #region Non Recursive PostOrder Traversal
+
+
+        #endregion
+
+
     }
 }
--- Algoritma/IleriSeviye/Tree/Tree.cs
+++ Algoritma/IleriSeviye/Tree/Tree.cs
@@ -11,75 +11,112 @@ namespace Programlama.IleriAlgoritma.Tree
             BST.Add(37);
             BST.Add(99);
 
-            var InOrderList = new BinaryTree<int>().InOrder(BST.Root);
+            var bt = new BinaryTree<int>();
+
+            //  InPrePostRecursiveTravels(BST, bt);
+            InPrePostNonRecursiveTravels(BST, bt);
+
+
+        }
+        private static void InPrePostNonRecursiveTravels(BST<int> BST, BinaryTree<int> bt)
+        {
+           var InOrderNonRecursiveList = bt.InOrderNonRecursiveTreversal(BST.Root!);
+            foreach (var item in InOrderNonRecursiveList)
+            {
+                System.Console.WriteLine(item);
+            }
+        }
+
+        private static void InPrePostRecursiveTravels(BST<int> BST, BinaryTree<int> bt)
+        {
+            //Inorder
+            var InOrderList = bt.InOrder(BST.Root);
 
             foreach (var item in InOrderList)
             {
-                System.Console.WriteLine(item.Value);
+                System.Console.Write(item.Value + " ");
             }
-            
-        }  
+
+            System.Console.WriteLine();
+
+            //Preorder
+            var PreOrderList = bt.PreOrder(BST.Root);
+
+            foreach (var item in PreOrderList)
+            {
+                System.Console.Write(item.Value + " ");
+            }
+
+            System.Console.WriteLine();
+
+            //PostOrder
+            var PostOrderList = bt.PostOrder(BST.Root);
+            foreach (var item in PostOrderList)
+            {
+                System.Console.Write(item + " ");
+            }
+        }
     }
-    
+
     #region Ağaç ve İkili Ağaç Tanım
-        /*
+    /*
 
-        *** 1. Ağaç (Tree) Nedir? ***
-                    
-                - Ağaç, düpümlerden(node) oluşan, hiyerarşik bir veri yapısıdır.
-                - En üstte kök(oot) düğüm vardır.
-                - Her düğümün 0 veya daha fazla alt düğümü (child) olabilir
-                - Alt düğümü olmayan düğümşere yaprak (leaf) denir.
-                - Düğümler arasındaki bağlantılara kenar (edge) denir.
+    *** 1. Ağaç (Tree) Nedir? ***
 
-            ---* Özellikleri:
-                    - Çizge teorisindeki özel bir graf yapısıdır (çevrim içermez)
-                    - Genelde hyerarşik yapıları temsil etmek için kullanılır: dosya sistemleri, organizasyon şemaları, oyun ağaçları
+            - Ağaç, düpümlerden(node) oluşan, hiyerarşik bir veri yapısıdır.
+            - En üstte kök(oot) düğüm vardır.
+            - Her düğümün 0 veya daha fazla alt düğümü (child) olabilir
+            - Alt düğümü olmayan düğümşere yaprak (leaf) denir.
+            - Düğümler arasındaki bağlantılara kenar (edge) denir.
 
-        *** 2. İkili Ağaç (Binary Tree)
-                
-                . Her düğümün en fazla 2 alt düğümü vardır:
-                    - Sol çocuk (left child)
-                    - Sağ çocuk (right child)
-                
-                . Boş olabilir (yani sol ve sağ çocuk olmayabilir)
-            
-            --* Özel Türleri:
+        ---* Özellikleri:
+                - Çizge teorisindeki özel bir graf yapısıdır (çevrim içermez)
+                - Genelde hyerarşik yapıları temsil etmek için kullanılır: dosya sistemleri, organizasyon şemaları, oyun ağaçları
 
-                    *-* Full Binary Tree ( Tam İkili Ağaç): Her düğümün ya 0 yada 2 çocuğu vardır
+    *** 2. İkili Ağaç (Binary Tree)
 
-                    *-* Complate Binary Tree (Tamamlanmış İkili Ağaç): Son seviye hariç tüm sevilerer tamamen doludur, son seviye soldan sağa soğru doldurulur.
+            . Her düğümün en fazla 2 alt düğümü vardır:
+                - Sol çocuk (left child)
+                - Sağ çocuk (right child)
 
-                    *-* Binary Search Tree, BTS ( İkili arama ağacı ): Sol alt ağaç kökten küçül, sağ alt ağaç kökten büpü değerleri tutar
+            . Boş olabilir (yani sol ve sağ çocuk olmayabilir)
 
-        *** 3. Ağaç Gzinmeleri (Tree Traversal)
+        --* Özel Türleri:
 
-                . Ağaç üzerindeki düpümleri belli bir sıraya göre dolaşmaya "treversal" denir.
-                . Trevarsal, Derinlik öncelikli (DFS) ve genişlik öncelikli (BFS) olarak ikiye ayrılır.
-            
-            A) Derinlik Öncelikli (DFS)
+                *-* Full Binary Tree ( Tam İkili Ağaç): Her düğümün ya 0 yada 2 çocuğu vardır
 
-                . İkli ağaçta üç temel yöntem vardır:
+                *-* Complate Binary Tree (Tamamlanmış İkili Ağaç): Son seviye hariç tüm sevilerer tamamen doludur, son seviye soldan sağa soğru doldurulur.
 
-                    1. Preorder (Kök - Sol - Sağ)
-                        . Önce kök , sonra sol alt ağaç, sora sağ alt ağaç
-                        . Kullanım: Ağaç kopyalama, ifade ağacından prefix ifede elde etme.
-                        . Örnek Sıralama: A -> B  -> D -> E -> C -> F -> G
-                    
-                    2. Inorder (Sol - Kök - Sağ)
-                        . Önce sol alt ağaç, sonra kök , sonra sağ alt ağaç
-                        . Kullanım: İkili arama ağacında sıralı çıktı verir
-                        . Örnek Sıralam: D -> B -> E -> A -> F -> C -> G
-                    
-                    3. Postorder (Sol - Sağ - Kök)
-                        . Önce sol, sonra sağ alt ağaç , en son kök
-                        . Kullanım: Bellek temizleme, ifade ağacından postfix ifade elde etme
-                        . Örnek Sıra D -> E -> B -> F -> G -> C -> A
-            
-            B) Genişlik Öncelikli (BFS / Level Order)
-                
-                . Ağaç seviye seviye gezilir (Queue kullanır)
-                . Örnek Sıra: A -> B -> C -> D -> E -> F -> G
-        */
+                *-* Binary Search Tree, BTS ( İkili arama ağacı ): Sol alt ağaç kökten küçül, sağ alt ağaç kökten büpü değerleri tutar
+
+    *** 3. Ağaç Gzinmeleri (Tree Traversal)
+
+            . Ağaç üzerindeki düpümleri belli bir sıraya göre dolaşmaya "treversal" denir.
+            . Trevarsal, Derinlik öncelikli (DFS) ve genişlik öncelikli (BFS) olarak ikiye ayrılır.
+
+        A) Derinlik Öncelikli (DFS)
+
+            . İkli ağaçta üç temel yöntem vardır:
+
+                1. Preorder (Kök - Sol - Sağ)
+                    . Önce kök , sonra sol alt ağaç, sora sağ alt ağaç
+                    . Kullanım: Ağaç kopyalama, ifade ağacından prefix ifede elde etme.
+                    . Örnek Sıralama: A -> B  -> D -> E -> C -> F -> G
+
+                2. Inorder (Sol - Kök - Sağ)
+                    . Önce sol alt ağaç, sonra kök , sonra sağ alt ağaç
+                    . Kullanım: İkili arama ağacında sıralı çıktı verir
+                    . Örnek Sıralam: D -> B -> E -> A -> F -> C -> G
+
+                3. Postorder (Sol - Sağ - Kök)
+                    . Önce sol, sonra sağ alt ağaç , en son kök
+                    . Kullanım: Bellek temizleme, ifade ağacından postfix ifade elde etme
+                    . Örnek Sıra D -> E -> B -> F -> G -> C -> A
+
+        B) Genişlik Öncelikli (BFS / Level Order)
+
+            . Ağaç seviye seviye gezilir (Queue kullanır)
+            . Örnek Sıra: A -> B -> C -> D -> E -> F -> G
+    */
     #endregion
 }
